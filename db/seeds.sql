5840b69df4d537d2b7ae96c5bb0851612ec0c718
Jeff Pearson <pjeffrey40@gmail.com>
2022-09-20 17:54:51 -0400

    finishing seeds

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+INSERT INTO department (name)
+VALUES (`Acting`),
+       (`Technical`),
+       (`Production`),
+       (`Casting`);
+
+INSERT INTO role (title, salary, department_id),
+VALUES (`Chief Creative Officer`, 2000000, 2),
+       (`Principle Director`, 23000000, 3),
+       (`Casting agent`, 1000000, 4),
+       (`Executive Producer`, 65000000, 3),
+       (`Lead Role`, 2000000, 1),
